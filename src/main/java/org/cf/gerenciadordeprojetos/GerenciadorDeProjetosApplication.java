963ae530e787733cedccc1656af43a5d91633092
GabrielCFeiten <gabrielcamposfeiten@gmail.com>
2026-03-22 15:17:20 -0300

    Model add

---
+++ src/main/java/org/cf/gerenciadordeprojetos/GerenciadorDeProjetosApplication.java
@@ -0,0 +1,13 @@
+package org.cf.gerenciadordeprojetos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeProjetosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GerenciadorDeProjetosApplication.class, args);
+    }
+
+}
